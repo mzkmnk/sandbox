addf5e89cc23e40450af6a59d5832da2a7649a81
mzkmnk <sma0729mex@gmail.com>
2024-11-25 23:01:54 +0900

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'tan-stack',
+    loadComponent:() => import('./pages/tan-stack.component').then((M) => M.TarnStackComponent),
+  },
+  {
+    path:'**',
+    redirectTo: 'tan-stack',
+  }
+];
