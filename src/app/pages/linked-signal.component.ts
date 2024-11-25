ddd94e390f9b81141a448e54926f391eb64deab4
mzkmnk <sma0729mex@gmail.com>
2024-11-25 23:02:20 +0900

    add:tan-stack & linkedSignal

---
+++ src/app/pages/linked-signal.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-linked-signal',
+  template:`
+    <h1>Linked Signal</h1>
+  `,
+})
+export class LinkedSignalComponent {
+
+}
---
+++ src/app/pages/tan-stack.component.ts
@@ -0,0 +1,45 @@
+import {Component, inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {injectQuery} from '@tanstack/angular-query-experimental';
+import {firstValueFrom} from 'rxjs';
+
+export type TPost = {
+  userId:number,
+  id:number,
+  title:string,
+  body:string,
+};
+
+@Component({
+  selector: 'app-tan-stack',
+  imports:[],
+  template:`
+    <h1>Tan Stack query</h1>
+
+    @let posts = query.data();
+    @let error = query.error;
+
+    @if(typeof posts !== 'undefined'){
+      @for(post of posts; let i = $index; track i){
+        <div>
+          <h3>{{post.title}}</h3>
+          <p>{{post.body}}</p>
+        </div>
+      }
+    }@else{
+      <div>
+        <p>loading...</p>
+      </div>
+    }
+  `
+})
+export class TarnStackComponent {
+  private readonly httpClient = inject(HttpClient);
+
+  query = injectQuery(() => ({
+    queryKey:['posts'],
+    queryFn:() => {
+      return firstValueFrom(this.httpClient.get<TPost[]>('https://jsonplaceholder.typicode.com/posts'))
+    },
+  }));
+}
