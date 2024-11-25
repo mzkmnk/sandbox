88f046ac0bbd07007d3d6912a734f1e8c19cea3b
mzkmnk <sma0729mex@gmail.com>
2024-11-26 07:59:36 +0900

    chore:linked-signal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     loadComponent:() => import('./pages/tan-stack.component').then((M) => M.TarnStackComponent),
   },
   {
+    path:'linked-signal',
+    loadComponent:() => import('./pages/linked-signal.component').then((M) => M.LinkedSignalComponent),
+  },
+  {
     path:'**',
     redirectTo: 'tan-stack',
   }
--- src/app/pages/linked-signal.component.ts
+++ src/app/pages/linked-signal.component.ts
@@ -1,11 +1,44 @@
-import {Component} from '@angular/core';
+import {Component, linkedSignal, signal} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-linked-signal',
-  template:`
+  template: `
     <h1>Linked Signal</h1>
+
+    <button (click)="onClickOp({op:'+'})">+</button>
+    <button (click)="onClickOp({op:'-'})">-</button>
+
+    <h3>Dynamic Signal Value :) {{ signalValue() }}</h3>
+
+    <textarea [(ngModel)]="textareaValue"></textarea>
+
+    <h3>text area value :) {{textareaValue()}}</h3>
+
+    <h3>if u used linkedSignal in form </h3>
+
+    <form>
+      <textarea [formControl]="textareaValueFormControl"></textarea>
+    </form>
   `,
+  imports: [
+    ReactiveFormsModule,
+    FormsModule
+  ]
 })
 export class LinkedSignalComponent {
+
+  signalValue = signal<number>(1);
+
+  onClickOp({op}:{op:'+'|'-'}){
+    this.signalValue.update((val) => op === '+' ? val+1 : val-1);
+  }
+
+  textareaValue = linkedSignal({
+    source:() => this.signalValue(),
+    computation:() => '',
+  })
+
+  textareaValueFormControl = new FormControl<string>('');
 
 }
